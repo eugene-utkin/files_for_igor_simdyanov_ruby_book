19fd99eb2c5ea06545eaabbb742332371196718a
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-12-29 16:23:05 +0500

    comment added

--- start/constant_change.rb
+++ start/constant_change.rb
@@ -3,4 +3,4 @@ puts CONST # 12
 CONST = 14
 # constant_change.rb:3: warning: already initialized constant CONST
 # constant_change.rb:1: warning: previous definition of CONST was here
-puts CONST
+puts CONST # 14
