23fd6cf3ee8151d1f7bd9ff94a0e994a6ad126b9
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-02-04 15:58:42 +0500

    Ticket class created

---
+++ variables/instance_variables.rb
@@ -0,0 +1,17 @@
+class Ticket
+  def set_date(date)
+    @date = date
+  end
+
+  def date
+    @date
+  end
+
+  def set_price(price)
+    @price = price
+  end
+
+  def price
+    @price
+  end
+end
