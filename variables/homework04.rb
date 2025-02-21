d9ec7d3bf7fcc130fd24ed71d530b5a2db6c3d34
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-02-21 19:21:12 +0500

    added default value to type parameter

--- variables/homework04.rb
+++ variables/homework04.rb
@@ -1,7 +1,7 @@
 class Car
   attr_accessor :brand, :model, :type, :color, :year
 
-  def initialize(brand: nil, model: nil, type, color, year)
+  def initialize(brand: nil, model: nil, type: nil, color, year)
 
   end
 end
