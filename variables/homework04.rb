cc65391b2953ea5c6250415dd5d04b895aaabf61
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-02-21 19:21:30 +0500

    added default value to color parameter

--- variables/homework04.rb
+++ variables/homework04.rb
@@ -1,7 +1,7 @@
 class Car
   attr_accessor :brand, :model, :type, :color, :year
 
-  def initialize(brand: nil, model: nil, type: nil, color, year)
+  def initialize(brand: nil, model: nil, type: nil, color: nil, year)
 
   end
 end
