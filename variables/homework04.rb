9ef7e933bad51d496c572ab3f142fa060b7e1727
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-02-21 19:21:53 +0500

    added default value to year parameter

--- variables/homework04.rb
+++ variables/homework04.rb
@@ -1,7 +1,7 @@
 class Car
   attr_accessor :brand, :model, :type, :color, :year
 
-  def initialize(brand: nil, model: nil, type: nil, color: nil, year)
+  def initialize(brand: nil, model: nil, type: nil, color: nil, year: nil)
 
   end
 end
