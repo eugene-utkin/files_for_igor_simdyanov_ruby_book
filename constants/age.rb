25ce7960724e19509cbacd20b5d70e40d933203b
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-03-25 21:18:27 +0500

    year error message modified

--- constants/age.rb
+++ constants/age.rb
@@ -19,6 +19,6 @@ end
 if ARGV[2] && ARGV[2].to_i.to_s == ARGV[2]
   year = ARGV[2].to_i
 else
-  puts "Wrong year! Enter birthdate in format dd mm yyyy"
+  puts "Wrong year format! Enter birthdate in format dd mm yyyy"
   exit
 end
