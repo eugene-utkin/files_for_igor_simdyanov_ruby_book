9b0eb2c3c36e72633fea24bced153f2c3ed326c9
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-04-24 20:16:55 +0500

    comment added

--- operators/format_integer.rb
+++ operators/format_integer.rb
@@ -3,4 +3,4 @@ number = 5867;
 puts '%b' % number  # 1011011101011
 puts '%b' % -number # ..10100100010101
 puts '%#b' % number # 0b1011011101011
-puts '%#B' % number
+puts '%#B' % number # 0B1011011101011
