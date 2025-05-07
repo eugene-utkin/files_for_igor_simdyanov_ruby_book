fe3e6b1e95b6aaaef38c34a7e38b2c9875dd68a1
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-05-07 18:32:58 +0500

    comment added

--- operators/compare_range.rb
+++ operators/compare_range.rb
@@ -3,4 +3,4 @@ p (1..10) === 11              # false
 p String  ==  'Hello, world!' # false
 p String  === 'Hello, world!' # true
 p /\d+/   ==  '12345'         # false
-p /\d+/   === '12345'
+p /\d+/   === '12345'         # true
