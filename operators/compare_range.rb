775dfff35fe58bb7ed4bb9f265ae66ed16193142
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-05-07 18:31:31 +0500

    comment added

--- operators/compare_range.rb
+++ operators/compare_range.rb
@@ -1,4 +1,4 @@
 p (1..10) === 5               # true
 p (1..10) === 11              # false
 p String  ==  'Hello, world!' # false
-p String  === 'Hello, world!'
+p String  === 'Hello, world!' # true
